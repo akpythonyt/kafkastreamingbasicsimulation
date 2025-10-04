113981bf056f0682fdb5e8aaddca15cff2e4aa80
Arunkumar <arun@Arunkumars-MacBook-Air.local>
2025-10-04 23:04:39 +0530

    Code modularisied

--- Kafkastreaming/Utils/config.py
+++ Kafkastreaming/Utils/config.py
@@ -1,9 +1,16 @@
 # config.py
-# Paths for Kafka and Zookeeper
+# Absolute paths to Kafka and scripts
 
-# Absolute path to your Kafka installation folder
-KAFKA_PATH = "users/arun/downloads/kafka_2.13-3.7.0"
+# Kafka installation path
+KAFKA_PATH = "/Users/arun/Downloads/kafka_2.13-3.7.0"
+
+# Project path (where producer and sparkstreaming scripts are located)
+PROJECT_PATH = "/Users/arun/Desktop/Dataingestion/Kafkastreaming"
 
 # Config files
 ZOOKEEPER_CONFIG = f"{KAFKA_PATH}/config/zookeeper.properties"
 KAFKA_CONFIG = f"{KAFKA_PATH}/config/server.properties"
+
+# Script paths
+PRODUCER_SCRIPT = f"{PROJECT_PATH}/producer.py"
+SPARK_SCRIPT = f"{PROJECT_PATH}/sparkstreaming.py"
